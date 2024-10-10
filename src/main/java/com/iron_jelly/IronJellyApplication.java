eeca88b055fa801d9f18bee7ac8921a1281e8f21
Askar.A <aalyiev7@gmail.com>
2024-10-10 15:49:08 +0500

    init commit

---
+++ src/main/java/com/iron_jelly/IronJellyApplication.java
@@ -0,0 +1,13 @@
+package com.iron_jelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronJellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronJellyApplication.class, args);
+	}
+
+}
