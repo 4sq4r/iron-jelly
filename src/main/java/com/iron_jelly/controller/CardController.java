b5a801399137240f3cf19cedd7debffcc2016bc4
Nikita <nick.bel1419@gmail.com>
2024-10-11 00:27:05 +0500

    добавил репозиторий card

---
+++ src/main/java/com/iron_jelly/controller/CardController.java
@@ -0,0 +1,11 @@
+package com.iron_jelly.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cards/v1")
+
+public class CardController {
+
+}
