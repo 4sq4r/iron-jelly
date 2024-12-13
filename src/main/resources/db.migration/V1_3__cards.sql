611f93991b67ed9bed462599022936b4c04bbdd8
Askar.A <aalyiev7@gmail.com>
2024-12-13 10:38:13 +0500

    changed migrations

--- src/main/resources/db.migration/V1_3__cards.sql
+++ src/main/resources/db.migration/V1_3__cards.sql
@@ -1,11 +1,13 @@
 create table if not exists cards
 (
     id               bigint primary key,
-    user_id          bigint    not null references users (id),
-    isActive         boolean   not null default true,
-    card_template_id bigint    not null references card_templates (id),
-    usage_limit      integer   not null default 1,
-    external_id      uuid      not null,
-    created_at       timestamp not null,
-    updated_at       timestamp not null
+    user_id          bigint      not null references users (id),
+    isActive         boolean     not null default true,
+    card_template_id bigint      not null references card_templates (id),
+    usage_limit      integer     not null default 1,
+    external_id      uuid        not null,
+    created_at       timestamp   not null,
+    updated_at       timestamp   not null,
+    created_by       varchar(50) not null default 'SYSTEM',
+    updated_by       varchar(50) not null default 'SYSTEM'
 )
--- src/main/resources/db.migration/V1_4__orders.sql
+++ src/main/resources/db.migration/V1_4__orders.sql
@@ -1,9 +1,11 @@
 create table if not exists orders
 (
     id          bigint primary key,
-    card_id     bigint    not null references cards (id),
-    is_free     boolean   not null default false,
-    external_id uuid      not null,
-    created_at  timestamp not null,
-    updated_at  timestamp not null
+    card_id     bigint      not null references cards (id),
+    is_free     boolean     not null default false,
+    external_id uuid        not null,
+    created_at  timestamp   not null,
+    updated_at  timestamp   not null,
+    created_by  varchar(50) not null default 'SYSTEM',
+    updated_by  varchar(50) not null default 'SYSTEM'
 )
